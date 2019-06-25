6d6077eb5344c4b5b2aea7180442949f50ccb44d
kuritaku61 <kuritaku.6111@gmail.com>
2019-06-25 12:17:00 +0900

    カスタムテンプレートのGridView　課題途中

---
+++ GridviewCustom/GridMApplyType.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GridMApplyType.aspx.cs" Inherits="kurihara_GridviewCustom_GridMApplyType" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+		</div>
+		<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="F_ID" DataSourceID="M_APPLY_TYPE">
+			<Columns>
+				<asp:TemplateField>
+					<HeaderTemplate>
+						ボタン
+					</HeaderTemplate>
+					<ItemTemplate>
+						<div>
+							<asp:Button ID="M_APPLY_TYPE" runat="server" Text="一覧表示" OnClick="M_APPLY_TYPE_Click" />
+						</div>
+					</ItemTemplate>
+				</asp:TemplateField>
+				<asp:BoundField DataField="F_ID" HeaderText="F_ID" InsertVisible="False" ReadOnly="True" SortExpression="F_ID" />
+				<asp:BoundField DataField="F_NAME" HeaderText="F_NAME" SortExpression="F_NAME" />
+			</Columns>
+		</asp:GridView>
+		<asp:SqlDataSource ID="M_APPLY_TYPE" runat="server" ConnectionString="<%$ ConnectionStrings:MyConnectionStr %>" SelectCommand="SELECT * FROM [M_APPLY_TYPE]"></asp:SqlDataSource>
+	</form>
+
+
+	F_ID
+	<asp:Label ID="F_ID1" runat="server" Text=""></asp:Label>
+	F_NAME
+	<asp:Label ID="F_NAME1" runat="server" Text=""></asp:Label>
+
+</body>
+</html>
---
+++ GridviewCustom/GridMApplyType.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class kurihara_GridviewCustom_GridMApplyType : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void SELECT_M_APPLY_TYPE(object sender, EventArgs e)
+    {
+        var args = (GridViewCommandEventArgs)e;
+
+		var index = int.Parse(args.CommandArgument.ToString());
+		var gridview = (GridView)args.CommandSource;
+		var id = int.Parse(gridview.DataKeys[index].Values["F_ID"].ToString());
+
+
+
+		string ConnectionStr = ConfigurationManager.ConnectionStrings["MyConnectionStr"].ConnectionString;
+		SqlConnection con = new SqlConnection(ConnectionStr);
+
+		con.Open();
+		try
+		{
+			// dbデータ取得
+			SqlCommand com = new SqlCommand("SELECT * FROM M_APPLY_TYPE WHERE F_ID = @id", con);
+			com.Parameters.Add(new SqlParameter("@id", id));
+			SqlDataReader sdr = com.ExecuteReader();
+
+			if (sdr.Read())
+			{
+
+				F_ID1.Text = sdr["F_ID"].ToString();
+				F_NAME1.Text = sdr["F_LAST_NAME"].ToString();
+				
+			}
+			com.Dispose();
+		}
+		catch
+		{
+			throw;
+		}
+		finally
+		{
+			con.Close();
+			con.Dispose();
+		}
+	}
+    
+
+    protected void M_APPLY_TYPE_Click(object sender, EventArgs e)
+    {
+        
+    }
+}
