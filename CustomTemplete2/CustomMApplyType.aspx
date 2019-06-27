d17b34eb7f294c253fa13d8fe657625bb4fa6a97
kuritaku61 <kuritaku.6111@gmail.com>
2019-06-27 10:25:04 +0900

    カスタムテンプレート2週目　一つ目途中

---
+++ CustomTemplete2/CustomMApplyType.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CustomMApplyType.aspx.cs" Inherits="kurihara_CustomTemplete2_CustomMApplyType" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+			<asp:GridView runat="server" ID="M_APPLY_TYPE" AutoGenerateColumns="false" OnRowCommand="M_APPLY_TYPE_RowCommand">
+				<Columns>
+					<asp:TemplateField>
+						<ItemTemplate>
+							<asp:Button ID="Select" Text="選択" runat="server" CommandName="SELECT" />
+						</ItemTemplate>
+					</asp:TemplateField>
+					<asp:TemplateField>
+						<HeaderTemplate>
+							F_ID
+						</HeaderTemplate>
+						<ItemTemplate>
+							<asp:Label ID="F_ID" Text="" runat="server"></asp:Label>
+						</ItemTemplate>
+					</asp:TemplateField>
+					<asp:TemplateField>
+						<HeaderTemplate>
+							F_NAME
+						</HeaderTemplate>
+						<ItemTemplate>
+							<asp:Label ID="F_NAME" Text="" runat="server"></asp:Label>
+						</ItemTemplate>
+					</asp:TemplateField>
+				</Columns>
+			</asp:GridView>
+		</div>
+	</form>
+</body>
+</html>
---
+++ CustomTemplete2/CustomMApplyType.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class kurihara_CustomTemplete2_CustomMApplyType : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string ConnectionStr = ConfigurationManager.ConnectionStrings["MyConnectionStr"].ConnectionString;
+        SqlConnection con = new SqlConnection(ConnectionStr);
+
+        con.Open();
+        try
+        {
+            DataTable table1 = new DataTable();
+            table1.Columns.Add(new DataColumn("F_ID", typeof(int)));
+            table1.Columns.Add(new DataColumn("F_NAME", typeof(string)));
+
+            SqlCommand com1 = new SqlCommand("SELECT * FROM M_APPLY_TYPE", con);
+            SqlDataReader sdr1 = com1.ExecuteReader();
+            while (sdr1.Read())
+            {
+                DataRow row = table1.NewRow();
+                row["F_ID"] = sdr1["F_ID"].ToString();
+                row["F_NAME"] = sdr1["F_NAME"].ToString();
+                table1.Rows.Add(row);
+            }
+            this.M_APPLY_TYPE.DataSource = table1;
+            this.M_APPLY_TYPE.DataBind();
+            sdr1.Close();
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            con.Close();
+            con.Dispose();
+        }
+    }
+
+
+    protected void M_APPLY_TYPE_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+
+    }
+}
